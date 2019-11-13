eca47cc8ca08fe7f36f7f24d0393a7e585097af6
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-11-13 17:50:14 -0500

    Implemented readPrincipals

--- IMDBparse.py
+++ IMDBparse.py
@@ -5,7 +5,8 @@ Internet Movie Database (IMDB)
 """
 
 #Reads title.basics.tsv,
-#stores as dict {title_id : movie_name}
+#stores as dict {title_id :[movie_name, actor_list]}
+# (note actor_list will be empty upon return)
 #return dict
 def readBasics():
   basics = open("title.basics.tsv", "r", encoding='utf-8')
@@ -21,6 +22,9 @@ def readBasics():
   #Key: title_ID
   #Value: title_string
   title_map = {}
+  
+  #Loop through lines, and add key,value
+  #pairs to title_map
   for entry in titles:
     
     #Split string by TAB character into
@@ -35,8 +39,54 @@ def readBasics():
     #key to title_map
     
     title_id = int((values[0][2:]))
-    title_map[title_id] = values[2]
+    title_map[title_id] = [values[2], [] ]
     
   return title_map
   
 
+def readPrincipals(basic_map):
+  principals = open("title.principals.tsv", "r", encoding='utf-8')
+  
+  #Read lines of file into entries
+  entries = principals.readlines()
+  
+  #Remove column names
+  del(entries[0])
+  
+  i = 0
+  for entry in entries:
+    
+    #See readBasics() for explanation
+    values = entry.split("\t")
+    title_id = int((values[0][2:]))
+    
+    #Add the person's unique identifier to the 
+    #second index of the value in basic_map at the
+    #key of the movie identifier
+    
+    
+    if title_id in basic_map:
+      basic_map[title_id][1].append(values[2])
+    else:
+      i+=1
+  print(i)
+test = readBasics()
+
+readPrincipals(test)
+
+print(test[67][1])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
