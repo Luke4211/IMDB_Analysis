7b14ab2e7f879fff5c6a89ec35209ef0a98592dd
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-11-14 01:43:02 -0500

    Implemented generate_adj_list

--- IMDBparse.py
+++ IMDBparse.py
@@ -1,12 +1,83 @@
 # -*- coding: utf-8 -*-
+import csv
+import os
 """
-IMDBparse.py ~ Contains functions for parsing the
-Internet Movie Database (IMDB)
+  IMDBparse.py ~ Contains functions for parsing the
+  Internet Movie Database (IMDB), encoding it into
+  a graph format, and writing the results to files.
 
-@author Lucas Gorski
+  @author Lucas Gorski
 """
 
-# Returns a list containing two dictionarys:
+# This is the primary function of this file.
+# Calling it parses all of the IMDB files, processes them into
+# an adjacency matrix and two reference maps, and then writes them
+# to files in the directory specified by directory_name.
+# 
+def generate_adj_list(directory_name):
+  os.mkdir(directory_name)
+  
+  # Call read_principals to parse IMDB data to
+  # generate the title and name maps
+  title_map, name_map = read_principals()
+  
+  # Open target files for writing
+  adj_list_file = open(directory_name + "/Adj_list.txt", "w", encoding='utf-8')
+  title_map_file = open(directory_name + "/Title_map.csv", "w", encoding='utf-8')
+  name_map_file = open(directory_name + "/Name_map.csv", "w", encoding='utf-8')
+  
+  # Pass map files to a csv writer, as we will
+  # store these as .csv files.
+  title_writer = csv.writer(title_map_file, delimiter=',', lineterminator='\n')
+  name_writer = csv.writer(name_map_file, delimiter=',', lineterminator='\n')
+  
+  # Write the contents of title_map and name_map to .csv files
+  # because we will be modifying their contents later
+  # in this function.
+  for key, value in title_map.items():
+    title_writer.writerow([key, value])
+  title_map_file.close()
+  
+  for key, value in name_map.items():
+    name_writer.writerow([key, value])
+  name_map_file.close()
+  
+  # Write entries from title_map to adjacency list file.
+  for key, value in title_map.items():
+    line = key + " "
+    for actor in value[1]:
+      line += actor + " "     
+      
+      # Remove edge from name_map to prevent
+      # duplicate edges from being recorded
+      name_map[actor][1].remove(key)
+    line += '\n'
+    adj_list_file.write(line)
+    
+  # Write entries from name_map to adjacency list file.
+  for key, value in name_map.items():
+    
+    if len(value[1]) > 0:
+      line = key + " "
+      
+      for movie in value[1]:
+        line += movie + " "    
+        
+      line += '\n'
+      adj_list_file.write(line)
+    else:
+      line = key + '\n'
+      adj_list_file.write(line)
+    
+  adj_list_file.close()
+
+
+'''
+  The functions below are helper functions for generate_adj_list
+'''
+
+
+# Returns a list containing two dictionaries:
 # The first dictionary maps movie ID's to their name, 
 # a list of actors/directors/writers who appear, the rating
 # of the movie, and the number of votes the rating is based off.
@@ -14,6 +85,7 @@ Internet Movie Database (IMDB)
 # The second dictionary maps people's ID's to their name and
 # a list of movies they appear in.
 def read_principals():
+  
   basic_map = read_basics()
   name_map = read_names()
   principals = open("title.principals.tsv", "r", encoding='utf-8')
@@ -36,7 +108,7 @@ def read_principals():
     
     # Check if title_id exists in basic map.
     # If it exists, update record. Otherwise do nothing.
-    # (Intuitively you would assume it does,
+    # (Intuitively you would assume it always does,
     # but as of 11/13/2019 there are 164 title_id's
     # present in title.principals.tsv that are not
     # present in title.basics.tsv)
@@ -46,12 +118,9 @@ def read_principals():
       # If it does not, do nothing.
       person_id = values[2]
       if person_id in name_map:
-        # Reference name_map by the person_id 
-        # to retrive actor's name. 
-        # Append name to actor_list in the 
-        # basic_map at key title_id
-        name = name_map[person_id][0]
-        basic_map[title_id][1].append(name)
+        # Append person_id to actor_list in the 
+        # basic_map at key title_i
+        basic_map[title_id][1].append(person_id)
         
         #Append title_id to the actor's movie list
         name_map[person_id][1].append(title_id)
@@ -65,7 +134,7 @@ def read_principals():
 #        a list of actors which appear in it, 
 #        it's rating out of 10 and how many votes
 #        the rating is based off.
-# (note actor_list will be empty upon return)
+# (note: the actor_list will be empty upon return)
 def read_basics():
   
   ratings_map = read_ratings()
@@ -142,21 +211,22 @@ def read_ratings():
     ratings_map[title_id] = [values[1], values[2].rstrip()]
   return ratings_map
 
-
+# Returns a dictionary in the following format:
+# Key: the person's unique ID
+# Value: list containing their name and a list
+#        containing all titles they appear in
+#        (Note: title list will be empty upon return)
 def read_names():
   names = open("name.basics.tsv", "r", encoding='utf-8')
   
   entries = names.readlines()
   
   names.close()
+  
   # Remove column names
   del(entries[0])
   
   # Initialize name_map to empty dictionary
-  # Key: the person's unique ID
-  # Value: list containing their name and a list
-  #        containing all titles they appear in
-  #        (Note: title list will be empty upon return)
   name_map = {}
   
   # Loop through entries for actors, and append
@@ -173,8 +243,5 @@ def read_names():
 
 
 
-basic_map, name_map = read_principals()
-
-print(basic_map["tt0829482"])
-print(name_map["nm1706767"])
+generate_adj_list("luck")
 
