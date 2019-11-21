9ead1e50785b9a260566c0d9573f9e051b87d95f
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-11-21 12:25:47 -0500

    Further testing

--- IMDBanalyze.py
+++ IMDBanalyze.py
@@ -31,19 +31,19 @@ def analyze_graph(directory_name):
   for com in comms:
     
     print("Community start: ")
-    j = 0
+    
     for node in com:
       
       
       if node in name_map:
         if i == 0:
-          temp[j] = name_map[node]
+          temp.append(str(name_map[node])
         print(str(name_map[node]))
       else:
         if i == 0:
-          temp[j] = title_map[node]
+          temp.append(str(title_map[node])
         print(str(title_map[node]))
-      j += 1
+      
     print("Community end \n\n")
     i += 1
   sg = graph.subgraph(comms[0])
