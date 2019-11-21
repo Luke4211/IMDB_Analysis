48c80f47f322b619104d93f8d3858cbc94fdb1e0
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-11-21 18:13:17 -0500

    Testing community finding

--- IMDBanalyze.py
+++ IMDBanalyze.py
@@ -37,11 +37,11 @@ def analyze_graph(directory_name):
       
       if node in name_map:
         if i == 0:
-          temp[j] = str(name_map[node])
+          temp[str(j)] = str(name_map[node])
         print(str(name_map[node]))
       else:
         if i == 0:
-          temp[j] = str(title_map[node])
+          temp[str(j)] = str(title_map[node])
         print(str(title_map[node]))
       j += 1
       
