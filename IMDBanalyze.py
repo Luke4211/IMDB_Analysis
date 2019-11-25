5d0eb944ccaa709c08ee84078acae51c976bb0c2
Lucas Gorski <lucascgorski@gmail.com>
2019-11-25 03:07:02 -0500

    Finished community finding

--- IMDBanalyze.py
+++ IMDBanalyze.py
@@ -15,47 +15,52 @@ from networkx.algorithms.smallworld import sigma
 import matplotlib.pyplot as plt
 import os
 def analyze_graph(directory_name):
-  graph_pkg = package_graph(directory_name)
-  graph = graph_pkg[0]
-  title_map = graph_pkg[1]
-  name_map = graph_pkg[2]
+
+  graph, title_map, name_map = package_graph(directory_name)
   
   #print("Closeness: " + nx.closeness_centrality(graph))
   print("Average Clustering Coefficient: " + str(nx.average_clustering(graph)))
   print("Density: " + str(nx.density(graph)))
   
-  #influence_max(graph, 5, title_map, name_map)
+  influence_max(graph, 2, title_map, name_map)
+  find_communities("comms", graph, title_map, name_map, 5)
+  
+  
+  
+
+# This function finds communities using the Clauset-Newman-Moore
+# greedy modularity maximation algorithm, and then creates
+# a new directory and fills it with image representations
+# of the first k communities
+def find_communities(directory_name, graph, title_map, name_map, k):
+
   comms = nx.algorithms.community.greedy_modularity_communities(graph)
-  
-  
-  
   i = 0
-  os.mkdir("communities")
+  os.mkdir(directory_name)
   for com in comms:
-    temp = {}
+    if i < k: 
+      temp = {}
     
-    for node in com:
-      
-      
-      if node in name_map:
-       
-        temp[node] = str(name_map[node])
+      for node in com:
+
+        if node in name_map:
+
+          temp[node] = str(name_map[node])
         
-      elif node in title_map:
-        temp[node] = str(title_map[node][0])
-    plt.figure(figsize=(15,15))
-    sg = graph.subgraph(com)
+        elif node in title_map:
+          temp[node] = str(title_map[node][0])
+      plt.figure(figsize=(15,15))
+      sg = graph.subgraph(com)
   
-    pos = nx.spring_layout(sg, k=1, iterations=50)
-    nx.draw_networkx_nodes(sg, pos, node_color='r', node_size=600, alpha=0.8)
+      pos = nx.spring_layout(sg, k=1, iterations=50)
+      nx.draw_networkx_nodes(sg, pos, node_color='r', node_size=600, alpha=0.8)
   
-    nx.draw_networkx_edges(sg, pos, width=1.0, alpha=0.5)
-    nx.draw_networkx_labels(sg, pos, temp, font_size=5)
+      nx.draw_networkx_edges(sg, pos, width=1.0, alpha=0.5)
+      nx.draw_networkx_labels(sg, pos, temp, font_size=5)
   
-    plt.savefig("communities/comm" + str(i) + ".png", dpi=800)
-    plt.close() 
-    i += 1
-  
+      plt.savefig(directory_name + "/community" + str(i) + ".png", dpi=1000)
+      plt.close() 
+      i += 1
 # Prints out maximally influential actors/movies.
 def influence_max(graph, num_select, title_map, name_map, prob=.1):
   sol, spread, elapsed, lookup = im.celf(graph, num_select, prob)
@@ -98,7 +103,7 @@ def package_graph(directory_name):
   graph = nx.read_adjlist(directory_name + "/Adj_list.txt")
   title_map, name_map = read_maps(directory_name)
   
-  return [graph, title_map, name_map]
+  return graph, title_map, name_map
 
 # Reads in title and name maps from specified directory,
 # returns a list containing each map
@@ -117,7 +122,4 @@ def read_maps(directory_name):
   title_map_file.close()
   name_map_file.close()
   
-  return [title_map, name_map]
-
-#analyze_graph("test")
-
+  return title_map, name_map
